fe030d98eb6f3d40a14b473ca243306cb3835893
shakzeeofficial <shakzee171@gmail.com>
2023-11-28 12:44:48 +0500

    finalized Named Arguments

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <?php
-function addOrConcatenate(int|string $a,int|string $b):int|string
-{
-    if(is_int($a) && is_int($b)){
-    return $a+$b;
-    }
-    else{
-        return $a.$b;
-    }
+
+echo substr(string:'John Wick',offset:1, length:5);
+function createAccount($name,$email,$password){
+    //create the account logic
 }
-var_dump(addOrConcatenate(2,'5'));
+
+createAccount(name:'John Wick',email:'john@gmail.com', password:'john123');
+die('');
+function makeCoffee($type,$sugar,$milk)
+{
+    echo $type;
+    echo '<br>';
+    echo $sugar;
+    echo '<br>';
+    echo $milk;
+    echo '<br>';
+}
+
+
+makeCoffee(type:'Espresso',sugar:2, milk:'Almond Milk');
+
 
 
