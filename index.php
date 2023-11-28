1b61972e5170f5ec5d99b10b085dbf4c3a86a341
shakzeeofficial <shakzee171@gmail.com>
2023-11-28 13:45:31 +0500

    finalized the Attributes in PHP 8

--- index.php
+++ index.php
@@ -1,16 +1,38 @@
 <?php
-$status = 522522;
-$message = match($status){
-    200,300=>'ok',
-    400=>'not found.',
-    500=>'Server Error',
-    default=> 'Default error here'
-};
+#[Attribute]
+class Important{
 
-$timeofDay = match('evening'){
-    'morning'=>'coffee',
-    'afternoon'=>'taa',
-    'evening'=>'milk',
-};
-echo $timeofDay;
+}
+
+class  Project {
+   #[Important]
+    public string $deadline;
+}
+
+$ReflectionClass = new ReflectionClass(Project::class);
+$properties = $ReflectionClass->getProperties();
+//var_dump($properties);
+foreach ($properties as $proprety){
+    $attributes = $proprety->getAttributes();
+    foreach ($attributes as $attribute){
+        echo $attribute->getName();
+    }
+}
+
+
+
+//$status = 522522;
+//$message = match($status){
+//    200,300=>'ok',
+//    400=>'not found.',
+//    500=>'Server Error',
+//    default=> 'Default error here'
+//};
+//
+//$timeofDay = match('evening'){
+//    'morning'=>'coffee',
+//    'afternoon'=>'taa',
+//    'evening'=>'milk',
+//};
+//echo $timeofDay;
 
